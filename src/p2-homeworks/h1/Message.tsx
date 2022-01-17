1946e4a2033b5ad3179d7a370ea4749878ad20e3
isefrog38 <isefroge38@gmail.com>
2022-01-17 16:17:09 +0300

    dz_2 isDone

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import c from './Message.module.css';
+import s from './Message.module.css';
 
 type MessageType = {
     avatar: string
@@ -10,19 +10,36 @@ type MessageType = {
 
 function Message(props: MessageType) {
     return (
-        <div className={c.AllBlock}>
-            <img className={c.card__image} src={props.avatar} alt=""/>
-            <div className={c.message}>
-                <div className={c.card}>
+        <div className={s.message}>
+            <img className={s.avatar} src={props.avatar} alt="avatar"/>
 
-                    <div className={c.box}>
-                        <h3>{props.name}</h3>
-                        <h5>{props.message} <span className={c.time}>{props.time}pm</span></h5>
-                    </div>
-                </div>
+            <div className={s.angle}></div>
+            <div className={s.content}>
+                <div className={s.name}>{props.name}</div>
+                <div className={s.text}>{props.message}</div>
+                <div className={s.time}>{props.time}pm</div>
             </div>
         </div>
     )
 }
 
 export default Message
+
+
+/*
+function Message(props: MessageType) {
+    return (
+        <div className={s.AllBlock}>
+            <img className={s.card__image} src={props.avatar} alt=""/>
+            <div className={s.message}>
+                <div className={s.card}>
+
+                    <div className={s.box}>
+                        <h3>{props.name}</h3>
+                        <h5>{props.message} <span className={s.time}>{props.time}pm</span></h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}*/
--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,19 +1,24 @@
 import React from 'react'
+import {AffairType} from "./HW2";
+import a from "./Affairs.module.css"
 
 type AffairPropsType = {
-    // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
+    affair: AffairType
+    deleteAffairCallback: (id: number) => void
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
+    const deleteCallback = () => {props.deleteAffairCallback(props.affair._id)}
+
+    const activeClass = a.item + " " ;
 
     return (
-        <div>
-            // show some text
+        <div className={a.affair}>
+            <div className={a.item}>{props.affair.name}</div>
+            <div className={activeClass + a[props.affair.priority]} >[{props.affair.priority}]</div>
 
-            <button onClick={deleteCallback}>X</button>
+            <button onClick={deleteCallback}
+                    className={activeClass + a.button} >X</button>
         </div>
     )
 }
