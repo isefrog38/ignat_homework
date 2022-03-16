8341c147eced6ef0be207cad831ea15f10831695
isefrog38 <isefroge38@gmail.com>
2022-03-16 22:40:36 +0300

    11 lesson is done

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,17 +1,17 @@
 import React, {useEffect, useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange';
 import s from './common/c7-SuperRange/SuperRange.module.css';
-import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange';
 import {restoreState, saveState} from "../h6/localStorage/localStorage";
+import {SuperDoubleRange} from "./common/c8-SuperDoubleRange/SuperDoubleRange";
 
 function HW11() {
-    const [value1, setValue1] = useState<number>(0)
-    const [value2, setValue2] = useState(100)
+    const [value, setValue] = useState<number>(0)
+    const [valueAll, setValueAll] = useState<number[]>([0, 100])
 
     useEffect(() => {
-        saveState("input_polzunok", value1);
-        setValue1(restoreState<number>("input_polzunok", +setValue1))
-    },[value1, value2])
+        saveState("input_polzunok", value);
+        setValue(restoreState<number>("input_polzunok", + setValue))
+    },[value])
 
     return (
         <>
@@ -19,20 +19,19 @@ function HW11() {
             <p/>
             <div className={s.main_block}>
                 <div className={s.input_and_text_block}>
-                    <span className={s.number}>{value1}</span>
+                    <span className={s.number}>{value}</span>
                     <SuperRange
-                        onChangeRange={setValue1}
-                        value={value1}
+                        onChangeRange={setValue}
+                        value={value}
                     />
                 </div>
-
                 <div className={s.input_and_text_block}>
-                    <span className={s.number}>{value1}</span>
-                    {/*<SuperDoubleRange
-                        onChangeRange={setValue2}
-                        value={value2}
-                    />*/}
-                    <span className={s.number}>{value2}</span>
+                    <span className={s.number}>{valueAll[0]}</span>
+                        <SuperDoubleRange
+                            onChangeRange={setValueAll}
+                            value={valueAll}
+                        />
+                    <span className={s.number}>{valueAll[1]}</span>
                 </div>
             </div>
             <hr/>
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,30 +1,61 @@
-import React, {ChangeEvent} from 'react'
-import {DefaultInputPropsType} from "../c7-SuperRange/SuperRange";
+import React, {useEffect} from 'react'
+import {Box, Slider} from "@mui/material";
 
-type SuperDoubleRangePropsType = DefaultInputPropsType & {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
-    // min, max, step, disable, ...
+type SuperDoubleRangePropsType = {
+    onChangeRange?: (value: Array<number>) => void
+    value?: Array<number>
 }
-
-const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
+export const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
         onChangeRange, value,
-        ...restProps
+
     }
 ) => {
 
+    const [value2, setValue2] = React.useState<Array<number>>([0, 100]);
+
+    useEffect(()=>{
+        debugger
+        if(value) {
+            setValue2(value)
+        }
+    },[value])
+
+    const minDistance = 10;
+
+    const handleChange2 = (
+        event: Event,
+        newValue: number | number[],
+        activeThumb: number,
+    ) => {
+        if (!Array.isArray(newValue)) {
+            return;
+        }
+        if (newValue[1] - newValue[0] < minDistance) {
+            if (activeThumb === 0) {
+                const clamped = Math.min(newValue[0], 100 - minDistance);
+                setValue2([clamped, clamped + minDistance]);
+            } else {
+                const clamped = Math.max(newValue[1], minDistance);
+                setValue2([clamped - minDistance, clamped]);
+            }
+        } else {
+            if (onChangeRange) {
+                onChangeRange(newValue)
+            }
+            setValue2(newValue as number[])
+        }
+    };
 
     return (
-        <>
-            <input
-                type={'range'}
-                /*onChange={onChangeCallback}*/
-
-                {...restProps}
+        <Box sx={{width: 120}}>
+            <Slider
+                getAriaLabel={() => 'Minimum distance shift'}
+                value={value2}
+                onChange={handleChange2}
+                valueLabelDisplay="auto"
+                disableSwap
             />
-        </>
+        </Box>
     )
 }
-
-export default SuperDoubleRange
